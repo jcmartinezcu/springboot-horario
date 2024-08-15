cdf4342a00547ea48591d9feb7020615c2de51ed
jcmartinez <jcmartinezcu@gmail.com>
2024-08-15 18:01:35 +0200

    implementando cerrando interceptor

--- src/main/java/com/jc/curso/springboot/calendar/interceptor/springboothorario/interceptors/CalendarInterceptor.java
+++ src/main/java/com/jc/curso/springboot/calendar/interceptor/springboothorario/interceptors/CalendarInterceptor.java
@@ -1,11 +1,17 @@
 package com.jc.curso.springboot.calendar.interceptor.springboothorario.interceptors;
 
-import org.apache.logging.log4j.util.StringBuilders;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -24,6 +30,7 @@ public class CalendarInterceptor implements HandlerInterceptor{
                 
                 Calendar calendar = Calendar.getInstance();
                 int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                System.out.println(hour);
 
                 if (hour>= open && hour< close){
                     StringBuilder message = new StringBuilder("Bienvenidos al horario de atencion al cliente");
@@ -38,6 +45,22 @@ public class CalendarInterceptor implements HandlerInterceptor{
                     request.setAttribute("message", message.toString());
                     return true;
                 }
+
+                ObjectMapper mapper = new ObjectMapper();
+                Map<String, Object> data = new HashMap<>();
+                StringBuilder message = new StringBuilder("Cerrado. fuera del horario de atención ");
+                message.append("por favor visitenos desde las ");
+                message.append(open);
+                message.append(" y las ");
+                message.append(close);
+                message.append("hrs. Gracias!");
+                data.put("message", message.toString());
+                data.put("date", new Date());
+
+                response.setContentType("application/json");
+                response.setStatus(401);
+                response.getWriter().write(mapper.writeValueAsString(data));
+
                 return false;
     }
 
